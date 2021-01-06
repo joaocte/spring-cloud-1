c97b37d1b91e16222cc8f37962528ce1d6b887cf
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-01-06 07:59:47 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/wego/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/wego/hrpayroll/resources/PaymentResource.java
@@ -1,12 +1,14 @@
 package com.wego.hrpayroll.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wego.hrpayroll.entities.Payment;
 import com.wego.hrpayroll.services.PaymentService;
 
@@ -17,9 +19,15 @@ public class PaymentResource  {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentFallbackMethod")
 	@GetMapping(value = "/worker/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		
 		return ResponseEntity.ok(paymentService.getPayment(workerId, days));
 	}
+	public ResponseEntity<Payment> getPaymentFallbackMethod(Long workerId, Integer days){
+		
+		return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null);
+	}
+	
 }
